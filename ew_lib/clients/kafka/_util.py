c663ed1c79a39fb77b17b1ac1cc7c3413368d314
y-du <42994541+y-du@users.noreply.github.com>
2022-03-09 15:14:33 +0100

    add consumer offset handler

--- ew_lib/clients/kafka/_util.py
+++ ew_lib/clients/kafka/_util.py
@@ -15,6 +15,7 @@
 """
 
 import ew_lib._util
+import confluent_kafka
 import typing
 
 
@@ -23,3 +24,22 @@ def log_kafka_sub_action(action: str, partitions: typing.List, prefix: str):
         ew_lib._util.logger.info(
             f"{prefix}: subscription event: action={action} topic={partition.topic} partition={partition.partition} offset={partition.offset}"
         )
+
+
+class ConsumerOffsetHandler:
+    def __init__(self, kafka_consumer: confluent_kafka.Consumer):
+        self.__kafka_consumer = kafka_consumer
+        self.__offsets = dict()
+
+    def add_offset(self, topic: str, partition: int, offset: int):
+        key = f"{topic}{partition}"
+        offset += 1
+        if key not in self.__offsets:
+            self.__offsets[key] = confluent_kafka.TopicPartition(topic=topic, partition=partition, offset=offset)
+        else:
+            tp_obj = self.__offsets[key]
+            tp_obj.offset = offset
+
+    def store_offsets(self):
+        if self.__offsets:
+            self.__kafka_consumer.store_offsets(offsets=list(self.__offsets.values()))
