8e01ae6035d150afaec90103bc713800a63c8dca
y-du <42994541+y-du@users.noreply.github.com>
2022-03-10 13:08:06 +0100

    bump version

--- ew_lib/__init__.py
+++ ew_lib/__init__.py
@@ -14,7 +14,7 @@
    limitations under the License.
 """
 
-__version__ = '0.15.2'
+__version__ = '0.15.3'
 __title__ = 'export-worker-lib'
 __description__ = 'Library for implementing export-workers to transfer data from the SENERGY streaming platform to data sinks.'
 __url__ = 'https://github.com/SENERGY-Platform/export-worker-lib'
