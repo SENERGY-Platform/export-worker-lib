aa7592d52136ed6b107bacf9c1d11123f435937a
y-du <42994541+y-du@users.noreply.github.com>
2022-03-22 15:28:16 +0100

    use latest

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
     url=metadata.get('__url__'),
     copyright=metadata.get('__copyright__'),
     install_requires=[
-        'message-filter-lib @ git+https://github.com/SENERGY-Platform/message-filter-lib.git@0.4.1',
+        'message-filter-lib @ git+https://github.com/SENERGY-Platform/message-filter-lib.git@0.5.1',
         'confluent_kafka>=1,<2'
     ],
     packages=setuptools.find_packages(exclude=("tests", "example")),
